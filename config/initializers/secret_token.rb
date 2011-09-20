531d9a795183498ed711b8f2b4f45f0ee6737b18
Shiv Bharthur <shiv.bharthur@gmail.com>
2011-09-20 18:25:06 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XperiaForum::Application.config.secret_token = '2558f820a52e19f1499764c9209f5a156a7e9baa75c73d2b7397b2aa6db559f29124ea5d2058dd60a5d2c4c562a3c50eae58a89fcf532d0166caba26260e98bd'
